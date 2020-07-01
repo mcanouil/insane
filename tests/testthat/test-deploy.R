c4e4dd767276957af7e87a7a18662c6a9894caea
Mickaël Canouil <8896044+mcanouil@users.noreply.github.com>
2020-07-01 16:12:01 +0200

    rename param

--- tests/testthat/test-deploy.R
+++ tests/testthat/test-deploy.R
@@ -2,6 +2,6 @@ test_that('deploy to temp dir', {
   dir.create(file.path(tempdir(), "test_insane"), showWarnings = FALSE)
   expect_true(deploy(directory = file.path(tempdir(), "test_insane")))
   expect_false(deploy(directory = file.path(tempdir(), "test_insane")))
-  expect_false(deploy(directory = file.path(tempdir(), "test_insane"), with_examples = TRUE))
-  expect_true(deploy(directory = file.path(tempdir(), "test_insane"), with_examples = TRUE, overwrite = TRUE))
+  expect_false(deploy(directory = file.path(tempdir(), "test_insane"), demo = TRUE))
+  expect_true(deploy(directory = file.path(tempdir(), "test_insane"), demo = TRUE, overwrite = TRUE))
 })
