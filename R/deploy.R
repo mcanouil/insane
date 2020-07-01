8c3a3024960cbb0ab93fdf1a78ca2e26fae8ac88
Mickaël Canouil <8896044+mcanouil@users.noreply.github.com>
2020-07-01 11:45:52 +0200

    add overwrite param

--- R/deploy.R
+++ R/deploy.R
@@ -4,7 +4,8 @@
 #'
 #' @param directory [[character]] A character vector of one path to the new location.
 #' @param app_name [[character]] A character vector defining the shiny application name in the new location.
-#' @param with_examples [[logical]] A boolean indicating if examples should be copied in the App.
+#' @param with_examples [[logical]] A logical indicating if examples should be copied in the App.
+#' @param overwrite [[logical]] A logical indicating if existing destination files should be overwritten.
 #'
 #' @return [[logical]] A logical indicating whether the deployment is successfull (`TRUE`) or not (`FALSE`).
 #' @export
@@ -17,12 +18,14 @@
 #'   shiny::runApp("insane")
 #' }
 #'
-deploy <- function(directory = "/srv/shiny-server", app_name = "insane", with_examples = FALSE) {
+deploy <- function(directory = "/srv/shiny-server", app_name = "insane", with_examples = FALSE, overwrite = FALSE) {
   dir.create(file.path(directory, app_name), showWarnings = FALSE, recursive = TRUE)
+  
   out <- all(file.copy(
     from = list.files(system.file("app", package = "insane"), full.names = TRUE),
     to = file.path(directory, app_name),
-    overwrite = TRUE, recursive = TRUE
+    overwrite = overwrite, 
+    recursive = TRUE
   ))
   
   dir.create(normalizePath(file.path(directory, app_name, "www", "xlsx")), showWarnings = FALSE)
@@ -31,7 +34,8 @@ deploy <- function(directory = "/srv/shiny-server", app_name = "insane", with_ex
     out <- all(c(out, file.copy(
       from = list.files(system.file("extdata", package = "insane"), full.names = TRUE, recursive = TRUE),
       to = file.path(directory, app_name, "www", "xlsx"),
-      overwrite = TRUE, recursive = TRUE
+      overwrite = overwrite, 
+      recursive = TRUE
     )))
   }
   
