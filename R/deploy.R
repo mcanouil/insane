acbcf063de126b215f0aec78894af9a977abff6b
Mickaël Canouil <mickael.canouil@cnrs.fr>
2020-07-02 16:48:21 +0200

    avoid non usefull folders to be copied

--- R/deploy.R
+++ R/deploy.R
@@ -20,9 +20,9 @@
 #'
 deploy <- function(directory = "/srv/shiny-server", app_name = "insane", demo = FALSE, overwrite = FALSE) {
   dir.create(file.path(directory, app_name), showWarnings = FALSE, recursive = TRUE)
-  
+
   out <- all(file.copy(
-    from = list.files(system.file("app", package = "insane"), full.names = TRUE),
+    from = list.files(system.file("app", package = "insane"), pattern = "app.R|www", full.names = TRUE),
     to = file.path(directory, app_name),
     overwrite = overwrite, 
     recursive = TRUE
